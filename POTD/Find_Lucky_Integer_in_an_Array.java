373591d2e5f885072fe458d51d372d7838eb532c
ankitklu <ankit.klu.2022@gmail.com>
2025-07-05 12:12:12 +0530

    potd day-3 done

---
+++ POTD/Find_Lucky_Integer_in_an_Array.java
@@ -0,0 +1,57 @@
+import java.util.*;
+class Solution {
+    public int findLucky(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int ele: arr){
+            map.put(ele, map.getOrDefault(ele,0)+1);
+        }
+
+        int maxEle=0;
+        int ele=-1;
+        for(Integer key: map.keySet()){
+            if(map.get(key)==key && key>ele){
+                ele=key;
+            }
+        }
+        return ele;
+    }
+}
+
+// 1394. Find Lucky Integer in an Array
+
+// avatar
+// Discuss Approach
+// arrow-up
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given an array of integers arr, a lucky integer is an integer that has a frequency in the array equal to its value.
+
+// Return the largest lucky integer in the array. If there is no lucky integer return -1.
+
+ 
+
+// Example 1:
+
+// Input: arr = [2,2,3,4]
+// Output: 2
+// Explanation: The only lucky number in the array is 2 because frequency[2] == 2.
+// Example 2:
+
+// Input: arr = [1,2,2,3,3,3]
+// Output: 3
+// Explanation: 1, 2 and 3 are all lucky numbers, return the largest of them.
+// Example 3:
+
+// Input: arr = [2,2,2,3,3]
+// Output: -1
+// Explanation: There are no lucky numbers in the array.
+ 
+
+// Constraints:
+
+// 1 <= arr.length <= 500
+// 1 <= arr[i] <= 500
