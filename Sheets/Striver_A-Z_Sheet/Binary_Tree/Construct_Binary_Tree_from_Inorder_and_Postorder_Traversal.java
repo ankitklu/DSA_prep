eb566c8c7b051526a3f5b25ddd3f547c1cddf7c5
ankitklu <ankit.klu.2022@gmail.com>
2025-08-14 23:14:34 +0530

    Binary Tree: Construct Binary Tree from Inorder and Postorder Traversal | Medium(66.7)| Striver

---
+++ Sheets/Striver_A-Z_Sheet/Binary_Tree/Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.java
@@ -0,0 +1,95 @@
+import java.util.*;
+/*
+ * 106. Construct Binary Tree from Inorder and Postorder Traversal
+
+avatar
+Discuss Approach
+arrow-up
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and postorder is the postorder traversal of the same tree, construct and return the binary tree.
+
+ 
+
+Example 1:
+
+
+Input: inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
+Output: [3,9,20,null,null,15,7]
+Example 2:
+
+Input: inorder = [-1], postorder = [-1]
+Output: [-1]
+ 
+
+Constraints:
+
+1 <= inorder.length <= 3000
+postorder.length == inorder.length
+-3000 <= inorder[i], postorder[i] <= 3000
+inorder and postorder consist of unique values.
+Each value of postorder also appears in inorder.
+inorder is guaranteed to be the inorder traversal of the tree.
+postorder is guaranteed to be the postorder traversal of the tree.
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        return helper(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1, map);
+    }
+
+    public TreeNode helper(int inorder[], int is, int ie, int postorder[], int ps, int pe,
+            HashMap<Integer, Integer> map) {
+        if (is > ie || ps > pe) {
+            return null;
+        }
+        TreeNode root = new TreeNode(postorder[pe]);
+        int inRoot = map.get(postorder[pe]);
+        int numsLeft = inRoot - is;
+        root.left = helper(inorder, is, inRoot - 1, postorder, ps, ps + numsLeft - 1, map);
+        root.right = helper(inorder, inRoot + 1, ie, postorder, ps + numsLeft, pe - 1, map);
+        return root;
+
+    }
+}
