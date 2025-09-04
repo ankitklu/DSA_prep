5cbe4d983f69834a52a7ef1c0e32f65160c081e3
ankitklu <ankit.klu.2022@gmail.com>
2025-09-04 23:13:39 +0530

    Math :POTD-04 | Find Closest Person | Easy(88.9)

---
+++ POTD/Find_Closest_Person.java
@@ -0,0 +1,13 @@
+public class Find_Closest_Person {
+    public int findClosest(int x, int y, int z) {
+        int p1=Math.abs(x-z);
+        int p2=Math.abs(y-z);
+         if(p1>p2){
+            return 2;
+         }
+         else if(p1<p2){
+            return 1;
+         }
+         return 0;        
+    }
+}
